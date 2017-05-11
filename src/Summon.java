f511eed0287757f6f70ed3f703c1edbff7ee9bfc
Deadspeck <thaone344@yahoo.com>
2017-05-11 13:35:22 -0600

    Initial commit

---
+++ src/Summon.java
@@ -0,0 +1,10 @@
+/**
+ * Created by compsci on 5/11/17.
+ */
+public class Summon {
+
+    public static void main(String[] args) {
+        System.out.println("Initial build 13:24 11 May 2017");
+    }
+
+}
